b3a9086126c3d3809b3ede05e16527e0921adff6
rafaelforcadell <rafael.forcadell@gmail.com>
2022-09-06 17:46:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-avfd9j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-avfd9j)
---
+++ src/app/tab-group-align-example.ts
@@ -0,0 +1,17 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+/**
+ * @title Tab group with aligned labels
+ */
+@Component({
+  selector: 'tab-group-align-example',
+  templateUrl: 'tab-group-align-example.html',
+  styleUrls: ['tab-group-align-example.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TabGroupAlignExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
